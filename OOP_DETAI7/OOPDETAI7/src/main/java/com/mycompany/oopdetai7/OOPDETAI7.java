3a327b66e079b5da00d5bea7e62f4d61f7852ebf
duyenphung1506 <24101771@st.phenikaa-uni.edu.vn>
2026-03-12 10:13:18 +0700

    Add files via upload

---
+++ OOP_DETAI7/OOPDETAI7/src/main/java/com/mycompany/oopdetai7/OOPDETAI7.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oopdetai7;
+
+/**
+ *
+ * @author PhungDuyen
+ */
+public class OOPDETAI7 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
