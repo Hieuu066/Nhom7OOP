41886ea56afab9a91ef083c0b1c4a9581a424643
hihiii <24100227@st.phenikaa-uni.edu.vn>
2026-03-13 17:09:06 +0700

    Add files via upload

---
+++ OOP_DETAI7/OOP_7/Account.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author TRUONG GIOI
+ */
+public class Account {
+    String accountNumber;
+    double balance;
+
+    public Account(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+}
---
+++ OOP_DETAI7/OOP_7/BankData.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author TRUONG GIOI
+ */
+import java.util.ArrayList;
+public class BankData {
+    public static ArrayList<Account> accounts = new ArrayList<>();
+    public static ArrayList<Transaction> transactions = new ArrayList<>();
+    public static TransactionHistoryForm historyForm = new TransactionHistoryForm();
+}
